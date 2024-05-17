932160ba5060f1c724c6b243b105b3c1d77019ef
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-05-17 20:25:50 +0900

    feat: 환전소 옵션 구현

--- src/main/java/casino/domain/options/ExchangeOption.java
+++ src/main/java/casino/domain/options/ExchangeOption.java
@@ -1,2 +1,29 @@
-package casino.domain.options;public enum ExchangeOption {
+package casino.domain.options;
+
+import java.util.Arrays;
+
+public enum ExchangeOption {
+    CHECK_ACCOUNT("1", "보유 잔액 확인"),
+    CASH_TO_CHIP("2", "현금 -> 칩 교환"),
+    CHIP_TO_CASH("3", "칩 -> 현금 교환"),
+    QUIT("Q", "나가기");
+
+    private final String optionNumber;
+    private final String command;
+
+    ExchangeOption(String optionNumber, String command) {
+        this.optionNumber = optionNumber;
+        this.command = command;
+    }
+
+    public static ExchangeOption from(String optionNumber) {
+        return Arrays.stream(values())
+                .filter(option -> option.optionNumber.equals(optionNumber))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 선택할 수 없는 기능입니다.\n"));
+    }
+
+    public boolean isContinue() {
+        return this != QUIT;
+    }
 }
