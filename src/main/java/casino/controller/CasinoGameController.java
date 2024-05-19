6b706d967fd9b1706c110c9465aba2c46647ffbf
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-05-19 20:38:10 +0900

    feat: 게임 옵션에 따른 게임 실행 기능 구현

--- src/main/java/casino/controller/CasinoGameController.java
+++ src/main/java/casino/controller/CasinoGameController.java
@@ -1,18 +1,22 @@
 package casino.controller;
 
 import casino.CasinoConfig;
-import casino.domain.options.ExchangeOption;
-import casino.domain.options.GameOption;
+import casino.domain.option.GameOption;
+import casino.domain.participant.Player;
+import casino.domain.participant.RoleType;
 import casino.io.game.GameInputView;
 import casino.io.game.GameOutputView;
+import casino.service.casino.CasinoMainService;
 
 public class CasinoGameController implements Controller {
     private final GameInputView gameInputView;
     private final GameOutputView gameOutputView;
+    private final CasinoMainService casinoMainService;
 
     public CasinoGameController(CasinoConfig casinoConfig) {
         this.gameInputView = casinoConfig.gameInputView();
         this.gameOutputView = casinoConfig.gameOutputView();
+        this.casinoMainService = casinoConfig.casinoMainService();
     }
 
     @Override
@@ -23,6 +27,34 @@ public class CasinoGameController implements Controller {
             gameOutputView.printBlankLine();
             gameOutputView.printGameOption();
             gameOption = gameInputView.readGameOption();
+            playGame(gameOption);
         } while (gameOption.isContinue());
+    }
+
+    private void playGame(GameOption gameOption) {
+        Player player = (Player) casinoMainService.findParticipantByRoleType(RoleType.PLAYER);
+
+        if (gameOption == GameOption.SLOT_MACHINE) {
+            playSlotMachine(player);
+        } else if (gameOption == GameOption.ROULETTE) {
+            playRoulette(player);
+        } else if (gameOption == GameOption.BLACKJACK) {
+            playBlackjack(player);
+        } else if (gameOption == GameOption.BACCARAT) {
+            playBaccarat(player);
+        }
+    }
+
+    private void playSlotMachine(Player player) {
+    }
+
+    private void playRoulette(Player player) {
+    }
+
+    private void playBlackjack(Player player) {
+    }
+
+    private void playBaccarat(Player player) {
+
     }
 }
